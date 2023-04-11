8c473c2afcfe251e9168966f93b607438b355fbc
Natalia392 <ntorrejonvalencia@gmail.com>
2023-04-11 15:41:29 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,16 @@
+import validator from './validator.js';
+
+console.log(validator);
+
+
+//Estoy tratando de declarar la variable de los números de la tarjeta
+
+let submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', () => {
+    let creditCardNumber = document.getElementById('cardNumber').value;
+
+} );
+
+
+
