1a3c30a7879470d802e9cdd1c98abecc7d6e1bb3
Natalia392 <ntorrejonvalencia@gmail.com>
2023-04-15 12:30:09 -0400

    Avance con función maskify completa y función isValid avanzada

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import validator from './validator.js';
 
+//Declaro la variable del botón
+const submitButton = document.getElementById('submitButton');
+
+//Creo una función para que al ingresar el input, éste sólo acepte números.
+/*function onlyNumbers() {
+  if ((submitButton.keyCode < 48) && (submitButton.keyCode > 57)) 
+  submitButton.returnValue = false;
+} */
+
+//Escucho el botón que envía los números de la tarjeta y guardo el valor recibido en la variable creditCardNumber
+submitButton.addEventListener('click', () => {
+  //Declaro la variable de los números de la tarjeta
+  const creditCardNumber = document.getElementById('cardNumber').value;
+  //Aplico la función maskify para los números ingresados
+  const mask = validator.maskify(creditCardNumber);
+  console.log(mask);
+  
+
+
+  // console.log(reversedArrayCreditCardNumber);
+  //Identifico qué números son pares y qué números son impares en el array
+
+  //Aplico la función isValid para los números ingresados
+  const valid = validator.isValid(creditCardNumber);
+  console.log(valid);
+});
+
+
 console.log(validator);
+
+
+
+
+
+
+
+
+/* const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  let creditCardNumber = document.getElementById('cardNumber').value;
+  console.log(creditCardNumber);
+});
+*/
+
+
+
+
+
+
+    
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
+
+  //Aquí va la función isValid
+  isValid: function(numberValidation) {
+    //Declaro la variable que transforma el string en números
+    const stringToNumber = parseInt(numberValidation);
+    //Declaro la variable que transforma los múmeros ingresados en un array
+    const createArray = stringToNumber.split('');
+    //Declaro la variable que reversa el array de números
+    const reverseArray = createArray.reverse();
+    //Creo una función para identificar qué números son pares y qué números son impares en el array
+    function pairIndex(arr) {
+      //Declaro el array vacío que luego contendrá los números en posiciones pares de la tarjeta
+      let pairIndexOfArray = [];
+      //Uso un for para agregar a un array todos los números en posiciones pares de la tarjeta
+      for (let i = 0; i < arr.lenght; i += 2) {
+        pairIndexOfArray = pairIndexOfArray.push(arr[i]);
+        //Si el producto entre el número y 2 es mayor o igual a 10
+        if ((arr[i]*2) >= 10) {
+          //Los dígitos del producto entre el número y dos se suman
+
+        }
+        //En caso contrario, sólo nos quedamos con el producto entre ese número y dos
+        else {
+          
+          // 
+        }
+      }
+      //Multiplico x2 los números de las posiciones impares y sumar sus dígitos si son mayores de 10
+    } 
+    pairIndex(reverseArray);
+    //Sumo los números de las posiciones impares y el nuevo número que se obtendrá de las posiciones pares.
+    //Aplico la función isValid para los números ingresados (esto se tiene que hacer en index.js)
+    // eslint-disable-next-line prefer-const
+    let validationContainer = false;
+    return validationContainer;
+  },
+  // Aquí va la función maskify
+  maskify: function(numbersToMaskify) {
+    //Números que se deben ocultar
+    const numbersToHide = numbersToMaskify.slice(0, -4);
+    //Números que se deben mostrar
+    const shownNumbers = numbersToMaskify.slice(-4);
+    //Tapar los números
+    const maskedNumbers = numbersToHide.replace(/./g, '#');
+    //Pido que me devuelva todos los números, primero los que se ocultan, luego los que se muestran.
+    return maskedNumbers + shownNumbers
+  }
 };
 
 export default validator;
