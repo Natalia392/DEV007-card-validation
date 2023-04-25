580ae975609641c807075db0aac7568465812b06
Natalia392 <ntorrejonvalencia@gmail.com>
2023-04-24 21:15:11 -0400

    Updates

--- index.js
+++ index.js
@@ -1,16 +1,81 @@
 import validator from './validator.js';
 
-console.log(validator);
+//Declaro la variable del botón
+const submitButton = document.getElementById('submitButton');
 
+const inputBox = document.getElementById('cardNumber'); //para obtener lo ingresado en el input
 
-//Estoy tratando de declarar la variable de los números de la tarjeta
+const justNumbers = document.getElementById('just-numbers');
 
-let submitButton = document.getElementById('submitButton');
+inputBox.addEventListener('input', (event) => {
+  const inputRegistered = event.target.value; //El input recibido 
+  const regex = /^[0-9]/g; //Búsqueda global de caracteres que no sean dígitos
+  inputBox.value = inputRegistered.replace(/\s/g, '').replace(/\D/g, ''); //Usando el método replace, se impide que ingrese spaces (\s) ni letras (\D)
 
+  if (regex.test(inputBox.value) === true) {
+    justNumbers.innerHTML = '';
+  } else {
+    justNumbers.innerHTML = 'Sólo puedes ingresar números';
+  }
+});
+
+//Escucho el botón que envía los números de la tarjeta y guardo el valor recibido en la variable creditCardNumber
 submitButton.addEventListener('click', () => {
-    let creditCardNumber = document.getElementById('cardNumber').value;
+  
+  const creditCardNumber = document.getElementById('cardNumber').value; //Declaro la variable de los números de la tarjeta
+  
+  const mask = validator.maskify(creditCardNumber); //Declaro la constante para mask los números ingresados
 
-} );
+  const valid = validator.isValid(creditCardNumber); //Declaro la constante para validar los números
+
+  const respuestaMask = document.getElementById('respuesta-mask'); //Para mostrar en pantalla los números enmascarados:
+
+  const isItValid = document.getElementById('isItValid');
+
+  //para que no acepte un input vacío:
+  if((creditCardNumber === '')) {
+    alert('Ingresa tu tarjeta')
+  } else {
+    //Para mostrar a la usuaria si su tarjeta es o no válida, el siguiente condicional:
+    if (valid === true) {
+
+      isItValid.innerHTML = 'Tu tarjeta es válida';
+      respuestaMask.innerHTML = 'Tarjeta ingresada: ' +  mask;
+
+    } else {
+
+      isItValid.innerHTML = 'Tu tarjeta es inválida';
+      respuestaMask.innerHTML = 'Tarjeta ingresada: ' +  mask;
+    }
+  }
+  
+  //console.log(creditCardNumber);
+});
+
+
+
+//console.log(validator);
+
+
+
+
+
+
+
+
+/* const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  let creditCardNumber = document.getElementById('cardNumber').value;
+  console.log(creditCardNumber);
+});
+*/
+
+
+
+
+
+
 
 
 
--- validator.js
+++ validator.js
@@ -1,5 +1,65 @@
 const validator = {
-  // ...
+
+  //Aquí va la función isValid
+  isValid: function(numberValidation) {
+
+    const createArray = numberValidation.split(''); //Declaro la variable que transforma los números ingresados en un array
+    
+    const arrayOfNumbers = createArray.map(Number); //Declaro la variable que transforma el array de string en array de números
+    
+    const reverseArray = arrayOfNumbers.reverse(); //Declaro la variable que reversa el array de números 
+    //console.log(reverseArray);
+    
+
+    //A partir del array reversado, saco los índices pares y los multiplico por 2
+    for (let i = 0; i < reverseArray.length; i++) {
+      //condición: 
+      if(i % 2 !== 0) {
+        reverseArray[i] = reverseArray[i]*2;
+
+        //numberx2 = reverseArray[i] //Agrego los índices pares a la constante declarada previamente
+        if (reverseArray[i] >= 10) {
+          let resultSum = 0;
+          const sum2Digits = reverseArray[i].toString().split('');
+          //console.log(sum2Digits + 'Los números de dos dígitos');
+          for(let i = 0; i < sum2Digits.length; i++) {
+            resultSum = resultSum + parseInt(sum2Digits[i]);
+          }
+          reverseArray[i] = resultSum;
+          resultSum = 0; //Para reinicar el resultSum en 0 al inicio del nuevo ciclo
+        }
+      }
+    }  
+      
+    let sumOfAllNumbers = 0; //Inicializo la variable que sumará todos los números
+    //sumo todos los números
+    for(let i = 0; i < reverseArray.length; i++) {
+      sumOfAllNumbers = sumOfAllNumbers + parseInt(reverseArray[i]);
+      //console.log((reverseArray[i]) + ' El array de los índices pares, multiplicados por 2');
+    }
+    
+    //console.log((sumOfAllNumbers) + ' Esta es mi suma');
+    
+    if (sumOfAllNumbers % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  // Aquí va la función maskify
+  maskify: function(numbersToMaskify) {
+    //Números que se deben ocultar
+    const numbersToHide = numbersToMaskify.slice(0, -4);
+    //Números que se deben mostrar
+    const shownNumbers = numbersToMaskify.slice(-4);
+    //Tapar los números
+    const maskedNumbers = numbersToHide.replace(/./g, '#');
+    //Pido que me devuelva todos los números, primero los que se ocultan, luego los que se muestran.
+    return maskedNumbers + shownNumbers;
+  },
 };
+
+
 
 export default validator;
